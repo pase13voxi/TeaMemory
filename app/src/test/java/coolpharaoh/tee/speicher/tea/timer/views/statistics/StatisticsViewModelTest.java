f56ccea3dc5c9f3e6bd68f13b3cc8b26120e2af4
pase13voxi <pascal_bechtoldt@web.de>
2020-11-06 10:47:42 +0100

    test

--- app/src/test/java/coolpharaoh/tee/speicher/tea/timer/views/statistics/StatisticsViewModelTest.java
+++ app/src/test/java/coolpharaoh/tee/speicher/tea/timer/views/statistics/StatisticsViewModelTest.java
@@ -1,11 +1,14 @@
 package coolpharaoh.tee.speicher.tea.timer.views.statistics;
 
+import android.app.Application;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.time.Duration;
 import java.time.Instant;
@@ -21,17 +24,24 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.powermock.api.mockito.PowerMockito.whenNew;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(fullyQualifiedNames = "coolpharaoh.tee.speicher.tea.timer.*")
 public class StatisticsViewModelTest {
     private StatisticsViewModel statisticsViewModel;
 
     @Mock
     CounterRepository counterRepository;
 
+    @Mock
+    Application application;
+
     @Before
-    public void setUp() {
-        statisticsViewModel = new StatisticsViewModel(counterRepository);
+    public void setUp() throws Exception {
+        whenNew(CounterRepository.class).withAnyArguments().thenReturn(counterRepository);
+
+        statisticsViewModel = new StatisticsViewModel(application);
     }
 
     @Test
