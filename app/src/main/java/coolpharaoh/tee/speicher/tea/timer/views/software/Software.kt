40b68023d29f7f1e1b0a745c09866a6c1d4fb8c0
pase13voxi <pase13voxi@users.noreply.github.com>
2023-07-23 12:55:23 +0200

    convert a view java classes to kotlin

--- app/src/main/java/coolpharaoh/tee/speicher/tea/timer/views/software/Software.kt
+++ app/src/main/java/coolpharaoh/tee/speicher/tea/timer/views/software/Software.kt
@@ -1,73 +1,83 @@
-package coolpharaoh.tee.speicher.tea.timer.views.software;
+package coolpharaoh.tee.speicher.tea.timer.views.software
 
-import android.os.Bundle;
-import android.widget.TextView;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.recyclerview.widget.DividerItemDecoration;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-
-import coolpharaoh.tee.speicher.tea.timer.R;
-import coolpharaoh.tee.speicher.tea.timer.views.utils.recyclerview.RecyclerItem;
-import coolpharaoh.tee.speicher.tea.timer.views.utils.recyclerview.RecyclerViewAdapter;
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import coolpharaoh.tee.speicher.tea.timer.R
+import coolpharaoh.tee.speicher.tea.timer.views.utils.recyclerview.RecyclerItem
+import coolpharaoh.tee.speicher.tea.timer.views.utils.recyclerview.RecyclerViewAdapter
+import java.util.Objects
 
 // This class has 9 Parent because of AppCompatActivity
-@SuppressWarnings("java:S110")
-public class Software extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(final Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_software);
-        defineToolbarAsActionbar();
-        enableAndShowBackButton();
-
-        configureAndShowListView();
+class Software : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_software)
+        defineToolbarAsActionbar()
+        enableAndShowBackButton()
+        configureAndShowListView()
     }
 
-    private void defineToolbarAsActionbar() {
-        final Toolbar toolbar = findViewById(R.id.tool_bar);
-        final TextView toolbarCustomTitle = findViewById(R.id.tool_bar_title);
-        toolbarCustomTitle.setText(R.string.software_heading);
-        setSupportActionBar(toolbar);
-        Objects.requireNonNull(getSupportActionBar()).setTitle(null);
+    private fun defineToolbarAsActionbar() {
+        val toolbar = findViewById<Toolbar>(R.id.tool_bar)
+        val toolbarCustomTitle = findViewById<TextView>(R.id.tool_bar_title)
+        toolbarCustomTitle.setText(R.string.software_heading)
+        setSupportActionBar(toolbar)
+        Objects.requireNonNull(supportActionBar)?.title = null
     }
 
-    private void enableAndShowBackButton() {
-        Objects.requireNonNull(getSupportActionBar()).setHomeButtonEnabled(true);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    private fun enableAndShowBackButton() {
+        Objects.requireNonNull(supportActionBar)?.setHomeButtonEnabled(true)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
     }
 
-    private void configureAndShowListView() {
-        final List<RecyclerItem> softwareList = generateListItems();
-
-        final RecyclerViewAdapter adapter = new RecyclerViewAdapter(R.layout.list_single_layout_software, softwareList,
-                position -> { /*this functionality is not needed, but needs to be override*/ });
-
-        final RecyclerView recyclerViewDetails = findViewById(R.id.recycler_view_software);
-        recyclerViewDetails.addItemDecoration(new DividerItemDecoration(recyclerViewDetails.getContext(), DividerItemDecoration.VERTICAL));
-        recyclerViewDetails.setLayoutManager(new LinearLayoutManager(this));
-        recyclerViewDetails.setAdapter(adapter);
+    private fun configureAndShowListView() {
+        val softwareList = generateListItems()
+        val adapter = RecyclerViewAdapter(
+            R.layout.list_single_layout_software, softwareList
+        ) { position: Int -> }
+        val recyclerViewDetails = findViewById<RecyclerView>(R.id.recycler_view_software)
+        recyclerViewDetails.addItemDecoration(
+            DividerItemDecoration(
+                recyclerViewDetails.context,
+                DividerItemDecoration.VERTICAL
+            )
+        )
+        recyclerViewDetails.layoutManager = LinearLayoutManager(this)
+        recyclerViewDetails.adapter = adapter
     }
 
-    private List<RecyclerItem> generateListItems() {
-        final List<RecyclerItem> softwareList = new ArrayList<>();
-        final RecyclerItem itemColorPicker = new RecyclerItem(getString(R.string.software_color_picker_heading), getString(R.string.software_color_picker_description));
-        softwareList.add(itemColorPicker);
-        final RecyclerItem itemChart = new RecyclerItem(getString(R.string.software_chart_heading), getString(R.string.software_chart_description));
-        softwareList.add(itemChart);
-        final RecyclerItem itemGlide = new RecyclerItem(getString(R.string.software_glide_heading), getString(R.string.software_glide_description));
-        softwareList.add(itemGlide);
-        final RecyclerItem itemJUnit = new RecyclerItem(getString(R.string.software_junit5_heading), getString(R.string.software_junit5_description));
-        softwareList.add(itemJUnit);
-        final RecyclerItem itemGson = new RecyclerItem(getString(R.string.software_gson_heading), getString(R.string.software_gson_description));
-        softwareList.add(itemGson);
-        return softwareList;
+    private fun generateListItems(): List<RecyclerItem> {
+        val softwareList: MutableList<RecyclerItem> = ArrayList()
+        val itemColorPicker = RecyclerItem(
+            getString(R.string.software_color_picker_heading),
+            getString(R.string.software_color_picker_description)
+        )
+        softwareList.add(itemColorPicker)
+        val itemChart = RecyclerItem(
+            getString(R.string.software_chart_heading),
+            getString(R.string.software_chart_description)
+        )
+        softwareList.add(itemChart)
+        val itemGlide = RecyclerItem(
+            getString(R.string.software_glide_heading),
+            getString(R.string.software_glide_description)
+        )
+        softwareList.add(itemGlide)
+        val itemJUnit = RecyclerItem(
+            getString(R.string.software_junit5_heading),
+            getString(R.string.software_junit5_description)
+        )
+        softwareList.add(itemJUnit)
+        val itemGson = RecyclerItem(
+            getString(R.string.software_gson_heading),
+            getString(R.string.software_gson_description)
+        )
+        softwareList.add(itemGson)
+        return softwareList
     }
 }
--- app/src/test/java/coolpharaoh/tee/speicher/tea/timer/views/software/SoftwareTest.kt
+++ app/src/test/java/coolpharaoh/tee/speicher/tea/timer/views/software/SoftwareTest.kt
@@ -1,73 +1,83 @@
-package coolpharaoh.tee.speicher.tea.timer.views.software;
+package coolpharaoh.tee.speicher.tea.timer.views.software
 
-import static android.os.Looper.getMainLooper;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.robolectric.Shadows.shadowOf;
+import android.os.Looper
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.core.app.ActivityScenario
+import coolpharaoh.tee.speicher.tea.timer.R
+import org.assertj.core.api.Assertions
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.Shadows
 
-import android.view.View;
-import android.widget.TextView;
-
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.test.core.app.ActivityScenario;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.robolectric.RobolectricTestRunner;
-
-import coolpharaoh.tee.speicher.tea.timer.R;
-
-@RunWith(RobolectricTestRunner.class)
-public class SoftwareTest {
-
-    private ActivityScenario<Software> softwareActivityScenario;
-
+@RunWith(RobolectricTestRunner::class)
+class SoftwareTest {
+    private var softwareActivityScenario: ActivityScenario<Software>? = null
     @Before
-    public void setUp() {
-        softwareActivityScenario = ActivityScenario.launch(Software.class);
+    fun setUp() {
+        softwareActivityScenario = ActivityScenario.launch(Software::class.java)
     }
 
     @Test
-    public void expectSoftwareList() {
-        softwareActivityScenario.onActivity(software -> {
-            final RecyclerView softwareRecyclerView = software.findViewById(R.id.recycler_view_software);
-
-            assertThat(softwareRecyclerView.getAdapter().getItemCount()).isEqualTo(5);
-
-            softwareRecyclerView.scrollToPosition(0);
-            shadowOf(getMainLooper()).idle();
-            checkHeaderAndPositionAtPositionInRecyclerView(softwareRecyclerView, 0,
-                    software.getString(R.string.software_color_picker_heading), software.getString(R.string.software_color_picker_description));
-
-            softwareRecyclerView.scrollToPosition(1);
-            shadowOf(getMainLooper()).idle();
-            checkHeaderAndPositionAtPositionInRecyclerView(softwareRecyclerView, 1,
-                    software.getString(R.string.software_chart_heading), software.getString(R.string.software_chart_description));
-
-            softwareRecyclerView.scrollToPosition(2);
-            shadowOf(getMainLooper()).idle();
-            checkHeaderAndPositionAtPositionInRecyclerView(softwareRecyclerView, 2,
-                    software.getString(R.string.software_glide_heading), software.getString(R.string.software_glide_description));
-
-            softwareRecyclerView.scrollToPosition(3);
-            shadowOf(getMainLooper()).idle();
-            checkHeaderAndPositionAtPositionInRecyclerView(softwareRecyclerView, 3,
-                    software.getString(R.string.software_junit5_heading), software.getString(R.string.software_junit5_description));
-
-            softwareRecyclerView.scrollToPosition(4);
-            shadowOf(getMainLooper()).idle();
-            checkHeaderAndPositionAtPositionInRecyclerView(softwareRecyclerView, 4,
-                    software.getString(R.string.software_gson_heading), software.getString(R.string.software_gson_description));
-        });
+    fun expectSoftwareList() {
+        softwareActivityScenario!!.onActivity { software: Software ->
+            val softwareRecyclerView =
+                software.findViewById<RecyclerView>(R.id.recycler_view_software)
+            Assertions.assertThat(softwareRecyclerView.adapter!!.itemCount).isEqualTo(5)
+            softwareRecyclerView.scrollToPosition(0)
+            Shadows.shadowOf(Looper.getMainLooper()).idle()
+            checkHeaderAndPositionAtPositionInRecyclerView(
+                softwareRecyclerView,
+                0,
+                software.getString(R.string.software_color_picker_heading),
+                software.getString(R.string.software_color_picker_description)
+            )
+            softwareRecyclerView.scrollToPosition(1)
+            Shadows.shadowOf(Looper.getMainLooper()).idle()
+            checkHeaderAndPositionAtPositionInRecyclerView(
+                softwareRecyclerView,
+                1,
+                software.getString(R.string.software_chart_heading),
+                software.getString(R.string.software_chart_description)
+            )
+            softwareRecyclerView.scrollToPosition(2)
+            Shadows.shadowOf(Looper.getMainLooper()).idle()
+            checkHeaderAndPositionAtPositionInRecyclerView(
+                softwareRecyclerView,
+                2,
+                software.getString(R.string.software_glide_heading),
+                software.getString(R.string.software_glide_description)
+            )
+            softwareRecyclerView.scrollToPosition(3)
+            Shadows.shadowOf(Looper.getMainLooper()).idle()
+            checkHeaderAndPositionAtPositionInRecyclerView(
+                softwareRecyclerView,
+                3,
+                software.getString(R.string.software_junit5_heading),
+                software.getString(R.string.software_junit5_description)
+            )
+            softwareRecyclerView.scrollToPosition(4)
+            Shadows.shadowOf(Looper.getMainLooper()).idle()
+            checkHeaderAndPositionAtPositionInRecyclerView(
+                softwareRecyclerView,
+                4,
+                software.getString(R.string.software_gson_heading),
+                software.getString(R.string.software_gson_description)
+            )
+        }
     }
 
-
-    private void checkHeaderAndPositionAtPositionInRecyclerView(final RecyclerView recyclerView, final int position,
-                                                                final String header, final String description) {
-        final View itemView = recyclerView.findViewHolderForAdapterPosition(position).itemView;
-        final TextView textViewHeading = itemView.findViewById(R.id.text_view_recycler_view_heading);
-        assertThat(textViewHeading.getText()).hasToString(header);
-        final TextView textViewDescription = itemView.findViewById(R.id.text_view_recycler_view_description);
-        assertThat(textViewDescription.getText()).hasToString(description);
+    private fun checkHeaderAndPositionAtPositionInRecyclerView(
+        recyclerView: RecyclerView, position: Int,
+        header: String, description: String
+    ) {
+        val itemView = recyclerView.findViewHolderForAdapterPosition(position)!!.itemView
+        val textViewHeading = itemView.findViewById<TextView>(R.id.text_view_recycler_view_heading)
+        Assertions.assertThat(textViewHeading.text).hasToString(header)
+        val textViewDescription =
+            itemView.findViewById<TextView>(R.id.text_view_recycler_view_description)
+        Assertions.assertThat(textViewDescription.text).hasToString(description)
     }
 }
