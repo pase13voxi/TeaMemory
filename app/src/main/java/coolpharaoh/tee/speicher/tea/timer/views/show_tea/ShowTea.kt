cbf65944dd21f9b0124df90b6ac9a9e62f7d6259
pase13voxi <pase13voxi@users.noreply.github.com>
2026-09-04 20:20:08 +0200

    chore: adjusted showtea

--- app/src/main/java/coolpharaoh/tee/speicher/tea/timer/views/show_tea/ShowTea.kt
+++ app/src/main/java/coolpharaoh/tee/speicher/tea/timer/views/show_tea/ShowTea.kt
@@ -134,7 +134,7 @@ class ShowTea : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_tea)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.show_tea_parent)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.show_tea_content)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
